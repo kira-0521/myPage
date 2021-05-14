85bbfc68d2ce273f7591717f0cde2401860f6845
kira-0521 <kiratanaka0521@gmail.com>
2021-05-14 19:21:06 +0900

    [add] コンポーネントを二つ作成
    フッターとラッパーのコンポーネントを作成

---
+++ components/LayoutFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="footer">
+    <p class="footer__text">Copyright &copy; Kira Tanaka</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'LayoutFooter',
+}
+</script>
