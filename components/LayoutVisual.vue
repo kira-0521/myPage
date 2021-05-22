7ad25d0829be81dc1c471eb2c818f3cea4a9d9ba
kira-0521 <kiratanaka0521@gmail.com>
2021-05-23 00:04:02 +0900

    [add]ダウンアロー
    ダウンアローを付与
    モバイルの時にスタイルが崩れる

--- components/LayoutVisual.vue
+++ components/LayoutVisual.vue
@@ -1,9 +1,10 @@
 <template>
   <section class="layoutVisual">
-    <div class="layoutVisual_textarea">
+    <div class="layoutVisual__inner">
       <h1 class="layoutVisual__title">{{ tagline }}</h1>
       <p class="layoutVisual__text">{{ text }}</p>
     </div>
+    <span class="arrow">scroll</span>
   </section>
 </template>
 
@@ -22,25 +23,63 @@ export default {
 @import './assets/css/modules/_variables.scss';
 
 .layoutVisual {
-  background-size: cover;
-  max-width: $contentMaxWidth;
   height: 90vh;
+  width: 100%;
   overflow: hidden;
-  padding: 7vw;
+  margin: 0;
 
-  &__textare {
-    max-width: 800px;
-    padding: 5% 3%;
+  &__inner {
+    max-width: $contentMaxWidth;
+    margin: 0 auto;
+    display: grid;
+    grid-template-rows: 1fr 2fr;
+    grid-template-columns: repeat(2, 1fr);
+    padding: 3%;
   }
 
   &__title {
     font-size: 38px;
     color: $cBlack;
+    grid-row: 1;
+    grid-column: 1 / 3;
   }
 
   &__text {
+    color: $cSecondText;
     display: inline-block;
     word-break: break-all;
+    grid-row: 2;
+    grid-column: 1 / 3;
+    position: relative;
+  }
+}
+
+.arrow {
+  display: inline-block;
+  position: absolute;
+  top: 55%;
+  left: 50%;
+  transform: translate(-50%, 0);
+  z-index: 2;
+  padding: 10px 10px 110px;
+  overflow: hidden;
+  color: $cBlack;
+  font-size: 14px;
+  font-family: 'Josefin Sans', sans-serif;
+  line-height: 1;
+  letter-spacing: 0.2em;
+  text-transform: uppercase;
+  text-decoration: none;
+  writing-mode: vertical-lr;
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    width: 1px;
+    height: 95px;
+    background-color: $cBlack;
   }
 }
 </style>
