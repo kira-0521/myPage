4e28dd51ce3d5e6e5bed1ede9f1cd16d79df6543
kira-0521 <kiratanaka0521@gmail.com>
2021-05-23 11:43:24 +0900

    [add]モバイルメニューの作成
    モバイルメニューのテンプレートを作成

---
+++ components/MobileMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mobile-menu">
+    <nav class="mobile-menu__nav">
+      <ul>
+        <li>
+          <nuxt-link to="/">Home</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/profile">Profile</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/blog">Blog</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/activitys">Activitys</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
