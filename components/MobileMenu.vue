ba977c58be2a0fb4cae8b9b9092e6eff091ee347
kira-0521 <kiratanaka0521@gmail.com>
2021-05-23 11:58:16 +0900

    [add]モバイルメニューテンプレート
    テンプレートのHTMLとスタイルを作成

--- components/MobileMenu.vue
+++ components/MobileMenu.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="mobile-menu">
     <nav class="mobile-menu__nav">
-      <ul>
-        <li>
+      <ul class="mobile-menu__ul">
+        <li class="mobile-menu__li">
           <nuxt-link to="/">Home</nuxt-link>
         </li>
-        <li>
+        <li class="mobile-menu__li">
           <nuxt-link to="/profile">Profile</nuxt-link>
         </li>
-        <li>
+        <li class="mobile-menu__li">
           <nuxt-link to="/blog">Blog</nuxt-link>
         </li>
-        <li>
+        <li class="mobile-menu__li">
           <nuxt-link to="/activitys">Activitys</nuxt-link>
         </li>
       </ul>
@@ -23,4 +23,28 @@
 export default {}
 </script>
 
-<style></style>
+<style scoped lang="scss">
+@import './assets/css/modules/_variables.scss';
+
+.mobile-menu {
+  height: 100vh;
+  width: 256px;
+  background-color: $cWhite;
+  transform: translateY(-100%);
+  top: 0;
+
+  &__nav {
+    padding: 8px 0;
+  }
+
+  &__ul {
+    display: flex;
+    flex-direction: column;
+    max-width: 100%;
+  }
+
+  &__li {
+    padding: 0 16px;
+  }
+}
+</style>
