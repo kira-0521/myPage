9cc9949b4d907b7a98737e4bdf90fd746c2c88c1
kira-0521 <kiratanaka0521@gmail.com>
2021-05-24 21:58:07 +0900

    [add]blog記事取得
    ブログの記事を非同期通信によって取得することに成功

---
+++ pages/_slug/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <layout-wrapper>
+    <main class="main">
+      <h1 class="title">{{ title }}</h1>
+      <p class="publishedAt">{{ publishedAt }}</p>
+      <!-- v-htmlを用いることで入ってきたbodyをDOMとして扱うことができる -->
+      <!-- eslint-disable-next-line vue/no-v-html -->
+      <div class="post" v-html="body"></div>
+    </main>
+  </layout-wrapper>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://mypage.microcms.io/api/v1/blog/${params.slug}`,
+      {
+        headers: { 'X-API-KEY': 'aebf3989-317c-41bd-ba6a-e969b4612cd2' },
+      }
+    )
+    // 帰ってきたオブジェクトのイチデータをコンポーネントにマージしている
+    return data
+  },
+}
+</script>
+
+<style></style>
