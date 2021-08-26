381afa8bc3a705599b564a0d4eb3666f40a38193
kira-giz <pyscripthon0426@gmail.com>
2021-08-27 08:08:41 +0900

    add: 各セクションタイトル用のコンポーネントを作成

---
+++ components/PagesTitle.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="title" :style="{ 'text-align': direction }">
+    <h2 class="title__title">{{ title }}</h2>
+    <p class="title__subtitle">{{ subtitle }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'About',
+    },
+    subtitle: {
+      type: String,
+      default: '私について',
+    },
+    direction: {
+      type: String,
+      default: 'center',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import './assets/css/modules/_variables.scss';
+
+.title {
+  &::after {
+    content: '';
+    display: inline-block;
+    margin-top: 12px;
+    height: 3px;
+    background-color: $cGreen;
+    width: 50px;
+  }
+
+  &__title {
+    font-size: 38px;
+    color: $cBlack;
+  }
+
+  &__subtitle {
+    font-size: 15px;
+    color: $cSecondText;
+  }
+}
+</style>
