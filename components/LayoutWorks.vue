035879ffc0b661020094f77d70ae9cba6f801b65
kira-giz <pyscripthon0426@gmail.com>
2021-08-28 13:15:18 +0900

    update: 基本スタイルの完成

--- components/LayoutWorks.vue
+++ components/LayoutWorks.vue
@@ -3,6 +3,19 @@
     <section class="works">
       <div class="inner works__inner">
         <PagesTitle :title="title" :subtitle="subtitle"></PagesTitle>
+        <ul class="works__img">
+          <template v-for="work in works">
+            <li :key="work.id" class="works__img-list">
+              <nuxt-img
+                :src="work.img"
+                quality="100"
+                class="inline-block"
+                preset="avatar"
+                sizes="md:100% lg:500px"
+              />
+            </li>
+          </template>
+        </ul>
       </div>
     </section>
   </layout-wrapper>
@@ -14,9 +27,34 @@ export default {
     return {
       title: 'Works',
       subtitle: '活動実績',
+      works: [
+        {
+          id: 1,
+          img: '/images/sample.jpg',
+        },
+        {
+          id: 2,
+          img: '/images/sample2.jpg',
+        },
+      ],
     }
   },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.works {
+  &__img {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    margin-top: 40px;
+
+    @media screen and (max-width: 769px) {
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+}
+</style>
