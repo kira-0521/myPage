79c095ce889c383b35fdb7fadf04cf97f5fb84a4
kira-giz <pyscripthon0426@gmail.com>
2021-08-29 13:06:11 +0900

    update: contactコンポーネントの追加

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,7 @@
   <layout-wrapper>
     <layout-visual />
     <layout-about />
+    <layout-works />
+    <layout-contact />
   </layout-wrapper>
 </template>
-
-<style lang="scss">
-ul {
-  list-style: none;
-  padding-left: 0;
-}
-.inner {
-  max-width: 1080px;
-  margin: 0 auto;
-}
-</style>
