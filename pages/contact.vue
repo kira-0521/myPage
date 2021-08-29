3a9405111983ce51a47b4253514679564644395b
kira-giz <pyscripthon0426@gmail.com>
2021-08-29 13:05:43 +0900

    update: contactコンポーネントの追加

---
+++ pages/contact.vue
@@ -0,0 +1,30 @@
+<template>
+  <layout-wrapper>
+    <layout-contact
+      :name.sync="form.name"
+      :email.sync="form.email"
+      :text.sync="form.text"
+      @email-send="emailSend"
+    ></layout-contact>
+  </layout-wrapper>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        email: '',
+        text: '',
+      },
+    }
+  },
+  methods: {
+    emailSend() {
+      // eslint-disable-next-line no-console
+      console.log(this.form)
+    },
+  },
+}
+</script>
