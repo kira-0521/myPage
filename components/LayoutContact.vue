d99d8d5d02db4f972ef5fefc97521ce13c19d13d
kira-giz <pyscripthon0426@gmail.com>
2021-08-29 13:04:48 +0900

    update: formのデータバインディングとスタイルの追加

---
+++ components/LayoutContact.vue
@@ -0,0 +1,45 @@
+<template>
+  <layout-wrapper>
+    <section class="contact">
+      <div class="inner contact__inner">
+        <PagesTitle :title="title" :subtitle="subtitle"></PagesTitle>
+        <Form
+          :name="name"
+          :email="email"
+          :text="text"
+          @emilSend="$emit('email-send')"
+        ></Form>
+      </div>
+    </section>
+  </layout-wrapper>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    email: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    text: {
+      type: String,
+      required: true,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      title: 'Contact',
+      subtitle: '連絡事項',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
