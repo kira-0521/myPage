b553ebdded979bc4c8062c646d097f4fa9694599
kira-giz <pyscripthon0426@gmail.com>
2021-09-18 17:03:18 +0900

    feat: モバイルメニューにトランジションと下線を追加して、左側から出るように設定

--- components/MobileMenu.vue
+++ components/MobileMenu.vue
@@ -30,12 +30,13 @@ export default {
   height: 100vh;
   width: 256px;
   background-color: $cWhite;
-  transform: translateX(100%);
+  transform: translateX(-100%);
   position: fixed;
   top: 0;
-  right: 0;
+  left: 0;
   z-index: 100;
   padding: 8px 0;
+  transition: all 0.5s;
 
   &__ul {
     display: flex;
@@ -46,6 +47,14 @@ export default {
   &__li {
     padding: 0 16px;
     margin: 16px 0;
+
+    &::after {
+      content: '';
+      height: 1px;
+      display: block;
+      margin-top: 10px;
+      background-color: $cSecondText;
+    }
   }
 }
 
