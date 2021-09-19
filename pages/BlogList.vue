f90c4a6bf96395bc290d7796b6de800c0a0bf310
kira-giz <pyscripthon0426@gmail.com>
2021-09-19 16:10:39 +0900

    feat: ブログデータを取ってくる処理を追加

---
+++ pages/BlogList.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  async asyncData({ $microcms }) {
+    const data = await $microcms.get({
+      endpoint: 'blog',
+      queries: { limit: 20, filters: 'createdAt[greater_than]2021' },
+    })
+    return data
+  },
+}
+</script>
--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -1,5 +1,19 @@
-<template></template>
+<template>
+  <div>
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="body"></div>
+  </div>
+</template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $microcms }) {
+    const data = await $microcms.get({
+      endpoint: 'blog',
+      contentId: 't3rh_dhooy',
+    })
+    return data
+  },
+}
 </script>
